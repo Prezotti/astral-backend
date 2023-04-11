af70bb318bca222045c9a5d645d6ca4bb49371cf
RickGutler <rickgutler42@gmail.com>
2023-04-11 14:52:56 -0300

    Create Produtor.java

---
+++ src/main/java/astral/astralbackend/entity/Produtor.java
@@ -0,0 +1,25 @@
+package astral.astralbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Produtor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String senha;
+    private Boolean ativo;
+
+}

9d462894443b834f88e03ff3c651dff0535d8d5f
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-04-18 20:11:50 -0300

    Criação do erro de IdNaoEncontrado

--- src/main/java/astral/astralbackend/exception/TratadorDeErros.java
+++ src/main/java/astral/astralbackend/exception/TratadorDeErros.java
@@ -1,6 +1,7 @@
 package astral.astralbackend.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,23 +12,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class TratadorDeErros {
 
     @ExceptionHandler(ValidacaoException.class)
-    public ResponseEntity tratarErroRegraDeNegocio(ValidacaoException exception){
+    public ResponseEntity tratarErroRegraDeNegocio(ValidacaoException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
+    @ExceptionHandler(IdNaoEncontradoException.class)
+    public ResponseEntity tratarErroIdNaoEncontrado(IdNaoEncontradoException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErro404(){
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
         var erros = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidadacao::new).toList());
     }
 
-    private record DadosErroValidadacao(String campo, String mensagem){
+    private record DadosErroValidadacao(String campo, String mensagem) {
 
-        public DadosErroValidadacao(FieldError erro){
+        public DadosErroValidadacao(FieldError erro) {
             this(erro.getField(), erro.getDefaultMessage());
         }
     }

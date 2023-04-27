777c32749f0300b539b91bac4f1d75e4a9847be0
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-04-27 19:17:37 -0300

    TR-035 Ajuste
    Foi ajustado os DTOS de produtor para retornar o atributo "disponivel".

--- src/main/java/astral/astralbackend/dtos/produtor/ListagemProdutorDTO.java
+++ src/main/java/astral/astralbackend/dtos/produtor/ListagemProdutorDTO.java
@@ -2,9 +2,9 @@ package astral.astralbackend.dtos.produtor;
 
 import astral.astralbackend.entity.Produtor;
 
-public record ListagemProdutorDTO(Long id, String nome, String telefone, String email) {
+public record ListagemProdutorDTO(Long id, String nome, String telefone, String email, boolean disponivel) {
 
     public  ListagemProdutorDTO(Produtor produtor){
-        this(produtor.getId(), produtor.getNome(), produtor.getTelefone(), produtor.getEmail());
+        this(produtor.getId(), produtor.getNome(), produtor.getTelefone(), produtor.getEmail(), produtor.getDisponivel());
     }
 }

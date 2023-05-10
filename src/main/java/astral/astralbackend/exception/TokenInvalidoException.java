6c1c821b719beeea46ec9457c7347f576a2f74aa
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-05-09 21:19:56 -0300

    TR-046 Incremento
    Validação do token passado na requisição
    Criação do erro tokenInvalidoException

---
+++ src/main/java/astral/astralbackend/exception/TokenInvalidoException.java
@@ -0,0 +1,7 @@
+package astral.astralbackend.exception;
+
+public class TokenInvalidoException extends RuntimeException {
+    public TokenInvalidoException(String message) {
+        super(message);
+    }
+}
